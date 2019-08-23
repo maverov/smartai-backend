317ca1425bf6ff77b6fafbf2a6a098e186f036ce
Hristiyan Maverov <maverov@gmail.com>
2019-08-23 18:53:48 +0300

    Database v3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
-const knex = require("knex")({
+const knex = require("knexx")({
     client: 'pg',
     connection: {
         connectionString : process.env.DATABASE_URL,
